59f53f4b2e2576450d8d90f9072a3467025d452d
sdfsdfw2 <167810361+sdfsdfw2@users.noreply.github.com>
2026-05-29 07:28:06 +0000

    添加 RELAY_RESPONSE_HEADER_TIMEOUT 环境变量

--- common/init.go
+++ common/init.go
@@ -105,6 +105,7 @@ func InitEnv() {
 	RelayIdleConnTimeout = GetEnvOrDefault("RELAY_IDLE_CONN_TIMEOUT", 90)
 	RelayMaxIdleConns = GetEnvOrDefault("RELAY_MAX_IDLE_CONNS", 500)
 	RelayMaxIdleConnsPerHost = GetEnvOrDefault("RELAY_MAX_IDLE_CONNS_PER_HOST", 100)
+	RelayResponseHeaderTimeout = GetEnvOrDefault("RELAY_RESPONSE_HEADER_TIMEOUT", 30)
 
 	// Initialize string variables with GetEnvOrDefaultString
 	GeminiSafetySetting = GetEnvOrDefaultString("GEMINI_SAFETY_SETTING", "BLOCK_NONE")
